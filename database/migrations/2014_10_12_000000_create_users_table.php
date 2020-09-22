71871aaaa1131d90b3f2b3bd6e36dc3713f59665
jack_zhu <thefallmoon@163.com>
2020-09-22 10:35:02 +0800

    first

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->string(‘weapp_openid’)->nullable()->comment(‘微信开放id’);
+            $table->string(‘weapp_session_key’)->nullable()->comment(‘微信session_key’);
+            $table->string(‘nickname’)->nullable()->comment(‘昵称’);
+            $table->string(‘weapp_avatar’)->nullable()->comment(‘微信头像’);
+            $table->string(‘country’)->nullable()->comment(‘国家’);
+            $table->string(‘province’)->nullable()->comment(‘省份’);
+            $table->string(‘city’)->nullable()->comment(‘所在城市’);
+            $table->string(‘language’)->nullable()->comment(‘语言’);
+            $table->json(‘location’)->nullable()->comment(‘当前地理信息’);
+            $table->enum(‘gender’, [‘1’, ‘2’])->default(‘1’)->comment(‘性别默认男’);
+            $table->string(‘phone’)->nullable()->unique();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
